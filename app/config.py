8ff1242dd7fa6b8ca0d634317717028b4ade9147
cdayz <cdayz@yandex.ru>
2020-09-17 15:43:31 +0300

    feat(app): Add config for construct database url

---
+++ app/config.py
@@ -0,0 +1,8 @@
+import os
+from urllib.parse import quote
+
+DB_USER = os.environ['DB_USER']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+DB_DSN = os.environ['DB_DSN']
+
+DB_URL = f'oracle+cx_oracle://{quote(DB_USER)}:{quote(DB_PASSWORD)}@{DB_DSN}'
