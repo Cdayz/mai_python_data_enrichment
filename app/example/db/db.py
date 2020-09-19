ebbace9b05f402e981ff2691262b2421b5d15f85
cdayz <cdayz@yandex.ru>
2020-09-19 14:14:31 +0300

    feat(example): Update database config

--- app/example/db/db.py
+++ app/example/db/db.py
@@ -3,8 +3,16 @@
 import os
 from urllib.parse import quote
 
+DB_SCHEME = os.environ['DB_SCHEME']
 DB_USER = os.environ['DB_USER']
 DB_PASSWORD = os.environ['DB_PASSWORD']
 DB_DSN = os.environ['DB_DSN']
 
-DB_URL = f'oracle+cx_oracle://{quote(DB_USER)}:{quote(DB_PASSWORD)}@{DB_DSN}'
+# Connection URL
+DB_URL = f'{DB_SCHEME}://{quote(DB_USER)}:{quote(DB_PASSWORD)}@{DB_DSN}'
+
+# DB_CN_ARGS = {'encoding': 'UTF-8', 'nencoding': 'UTF-8'}
+# DB_KWARGS = {'max_identifier_length': 128}
+
+DB_CN_ARGS: dict = {}
+DB_KWARGS: dict = {}
